c6d68ea48f0ce1c46e08da7ef5cc8d6088778199
DarkLord1731 <darklord1731@gmail.com>
2018-03-21 18:23:44 +0530

    on7xelte: Configure for DotOS

--- dot.mk
+++ dot.mk
@@ -20,11 +20,11 @@ $(call inherit-product, device/samsung/on7xelte/device.mk)
 # Inherit from those products. Most specific first
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common DotOS stuff
+$(call inherit-product, vendor/dot/config/common.mk)
 
 # Device identifier
-PRODUCT_NAME := lineage_on7xelte
+PRODUCT_NAME := dot_on7xelte
 PRODUCT_DEVICE := on7xelte
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
