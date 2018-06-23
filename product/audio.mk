d75d1f546b616ee180d5712650be44ecb6dbf41c
DarkLord1731 <darklord1731@gmail.com>
2018-06-23 20:43:47 +0530

    Revert "on7xelte: Move to stock audio HAL"
    This reverts commit 25c791bf0676589a9bba45f46871fe055b14293d.

--- product/audio.mk
+++ product/audio.mk
@@ -17,6 +17,4 @@
 # Audio Configs
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/configs/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
-    $(LOCAL_PATH)/configs/audio/mixer_paths_rev03.xml:system/etc/mixer_paths_rev03.xml
-
+    $(LOCAL_PATH)/configs/audio/mixer_paths_0.xml:system/vendor/etc/mixer_paths_0.xml
