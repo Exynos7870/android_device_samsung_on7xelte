08d1c2aeb924dabefcb0b63646a317bdfb3ab97b
DarkLord1731 <darklord1731@gmail.com>
2018-08-12 14:46:23 +0530

    on7xelte: Switch to 32bit fp hidl

--- product/fingerprint.mk
+++ product/fingerprint.mk
@@ -19,5 +19,4 @@ PRODUCT_PACKAGES += \
     mcDriverDaemon \
     fingerprint.exynos5 \
     libbauthtzcommon_shim \
-    android.hardware.biometrics.fingerprint@2.1-impl \
-    android.hardware.biometrics.fingerprint@2.1-service
+    android.hardware.biometrics.fingerprint@2.1-impl
